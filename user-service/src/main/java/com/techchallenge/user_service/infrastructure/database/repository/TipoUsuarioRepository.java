edcd734324d35f872f2fa5ecc54930ca0208bebb
felipesora <felipeusora@gmail.com>
2026-08-05 21:37:47 -0300

    feat: criando um data seeder dos tipos de usuário ao iniciar a aplicação

--- user-service/src/main/java/com/techchallenge/user_service/infrastructure/database/repository/TipoUsuarioRepository.java
+++ user-service/src/main/java/com/techchallenge/user_service/infrastructure/database/repository/TipoUsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.techchallenge.user_service.infrastructure.database.repository;
 
+import com.techchallenge.user_service.domain.entity.TipoUsuarioEnum;
 import com.techchallenge.user_service.infrastructure.database.entity.TipoUsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,6 @@ import java.util.UUID;
 
 @Repository
 public interface TipoUsuarioRepository extends JpaRepository<TipoUsuarioEntity, UUID> {
+
+    boolean existsByTipo(TipoUsuarioEnum tipo);
 }
